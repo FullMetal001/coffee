f9eee474ffb8cbd9b3b2f8f721927e9df6da3198
github_FullMetal001 <tantun_gel@hotmail.com>
2024-10-15 13:50:33 -0400

    verificar

--- src/complementos/Direccion.jsx
+++ src/complementos/Direccion.jsx
@@ -1,5 +1,22 @@
+import React, { useEffect, useState } from 'react';
 
 const Direccion = () => {
+    const [maxWidth, setMaxWidth] = useState(window.innerWidth); // Estado para almacenar el ancho de la ventana
+
+    // Función para actualizar el ancho
+    const updateWidth = () => {
+        setMaxWidth(window.innerWidth);
+    };
+
+    // useEffect para agregar y limpiar el event listener
+    useEffect(() => {
+        window.addEventListener('resize', updateWidth); // Agrega el event listener
+
+        // Limpieza del event listener al desmontar el componente
+        return () => {
+            window.removeEventListener('resize', updateWidth);
+        };
+    }, []);
   return (
     <div className="d-container">
         <div className="d-fondo">
@@ -7,8 +24,9 @@ const Direccion = () => {
         </div>
         <div className="d-contenido">
             <img src="/images/loguito.png" alt="c-logo" />
-            <h2>SCHEDULE AND LOCATION</h2>            
-        </div>
+            <h2>SCHEDULE AND LOCATION</h2>  
+            hol: {maxWidth}         
+        </div>       
         <div className="d-lugar">
             <p>ALL WEEK 7:30 AM TO 7:00 PM</p>
             <p>NW 17th St with NW 34th Ave.</p>
@@ -22,8 +40,8 @@ const Direccion = () => {
                     referrerPolicy="no-referrer-when-downgrade">
                 </iframe>
             </div>
-        </div>
-    </div>
+        </div>        
+    </div>   
   )
 }
 
